6ebc50602736fd28dd05c4fff8a06cd9ac5c0590
DimitriosMalonas <dimitriosmalonas@outlook.com>
2020-01-25 03:04:45 +0000

    Find maximum number of items that fit in budget method

--- ArrayRotations/src/SortingMethods.java
+++ ArrayRotations/src/SortingMethods.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 
 public class SortingMethods {
 
 	//https://www.hackerrank.com/challenges/ctci-bubble-sort/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
-    static void countSwapsInBubbleSort(int[] a) {
+    public void countSwapsInBubbleSort(int[] a) {
         int swaps = 0, temp;
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length - 1; j++) {
@@ -19,6 +20,24 @@ public class SortingMethods {
                          "First Element: " + a[0] + "\n" +
                          "Last Element: " + a[a.length - 1] + "\n");
 
+    }
+    
+    
+    
+    
+    //https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=sorting
+    public int maximumToys(int[] prices, int k) {
+        Arrays.sort(prices);
+        int cnt = 0;
+        for (int itemPrice : prices) {
+            if((k - itemPrice) < 0) {
+            	break;
+            } else {
+            	k -= itemPrice;
+            	cnt++;
+            }
+        }
+		return cnt;
     }
 
 }
